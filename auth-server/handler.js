f6aad38e2cde7958dc6a0a7ece809ce9518938ff
Linh Pham <77943437+cluelesslinh@users.noreply.github.com>
2021-09-06 20:28:31 -0700

    handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -36,26 +36,46 @@ const oAuth2Client = new google.auth.OAuth2(
  * as a URL parameter.
  *
  */
-module.exports.getAuthURL = async (event) => {
+module.exports.getAuthURL = async () => {
+  /**
+   *
+   * Scopes array passed to the `scope` option. Any scopes passed must be enabled in the
+   * "OAuth consent screen" settings in your project on your Google Console. Also, any passed
+   *  scopes are the ones users will see when the consent screen is displayed to them.
+   *
+   */
 
- // The values used to instantiate the OAuthClient are at the top of the file
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
 
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({
+      authUrl: authUrl,
+    }),
+  };
+};
+
+module.exports.getAccessToken = async (event) => {
+// The values used to instantiate the OAuthClient are at the top of the file
   const oAuth2Client = new google.auth.OAuth2(
     client_id,
     client_secret,
     redirect_uris[0]
   );
-
   // Decode authorization code extracted from the URL query
-
   const code = decodeURIComponent(`${event.pathParameters.code}`);
 
   return new Promise((resolve, reject) => {
-
     /**
-      *  Exchange authorization code for access token with a “callback” after the exchange,
-      *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
-      */
+     *  Exchange authorization code for access token with a “callback” after the exchange,
+     *  The callback in this case is an arrow function with the results as parameters: “err” and “token.”
+     */
 
     oAuth2Client.getToken(code, (err, token) => {
       if (err) {
@@ -65,16 +85,21 @@ module.exports.getAuthURL = async (event) => {
     });
   })
     .then((token) => {
+      // Respond with OAuth token
       return {
         statusCode: 200,
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
         body: JSON.stringify(token),
       };
     })
     .catch((err) => {
+      // Handle error
       console.error(err);
       return {
         statusCode: 500,
         body: JSON.stringify(err),
       };
     });
-  };
+};
