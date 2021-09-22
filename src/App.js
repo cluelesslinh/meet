bb90162dd89886bf54be81664dafe6f2ef681ed4
Linh Pham <77943437+cluelesslinh@users.noreply.github.com>
2021-09-22 14:59:38 -0700

    4.4 Integration Testing
    Added eventCount and locations to state

--- src/App.js
+++ src/App.js
@@ -8,43 +8,61 @@ import './nprogress.css';
 
 class App extends Component {
 
-  state = {
-    events: [],
-    locations: []
-  }
+ state = {
+   events: [],
+   locations: [],
+   numberOfEvents: 32,
+   currentLocation: 'all'
+ }
 
-  componentDidMount() {
-    this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({ events, locations: extractLocations(events) });
-      }
-    });
-  }
+ componentDidMount() {
+  this.mounted = true;
+  getEvents().then((events) => {
+    if (this.mounted) {
+      this.setState({
+        events: events.slice(0, this.state.numberOfEvents),
+        locations: extractLocations(events)
+      });
+     }
+   })
+   .catch(error => {
+    console.log(error);
+  })
+}
 
   componentWillUnmount() {
     this.mounted = false;
   }
 
-  updateEvents = (location) => {
-  getEvents().then((events) => {
-    const locationEvents = (location === 'all') ?
-      events :
-      events.filter((event) => event.location === location);
-    this.setState({
-      events: locationEvents
-    });
-  });
-}
+ updateEvents = (location, eventCount) => {
+   getEvents().then((events) => {
+     let locationEvents = (location === 'all') ?
+       events:
+       events.filter((event) => event.location === location);
+     locationEvents = locationEvents.slice(0, eventCount)
+     this.setState({
+       events: locationEvents,
+       currentLocation: location
+     });
+   });
+ }
+
+ updateEventCount = (eventCount) => {
+   this.setState({
+     numberOfEvents: eventCount
+   });
+   const { currentLocation } = this.state;
+   this.updateEvents(currentLocation, eventCount);
+ };
 
   render() {
+    const { numberOfEvents, locations, events } = this.state;
     return (
       <div className="App">
-        <CitySearch
-          locations={this.state.locations}
-          updateEvents={this.updateEvents} />
-        <EventList events={this.state.events} />
-        <NumberOfEvents />
+        <div className="navbar"></div>
+        <CitySearch locations={locations} updateEvents={this.updateEvents} />
+        <EventList events={events}/>
+        <NumberOfEvents updateEventCount={(e) => this.updateEventCount(e)} numberOfEvents={numberOfEvents} />
       </div>
     );
   }
