b2454f643cb5ee33870db069804928bb9241b705
Linh Pham <77943437+cluelesslinh@users.noreply.github.com>
2021-09-22 14:54:28 -0700

    Update NumberOfEvents.test.js
    added more tests

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -2,15 +2,30 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents';
 
-describe('<NumberOfEvents /> component', () => {
-
-  let numberFilter;
-
+describe("<NumberOfEvents /> component", () => {
+  let NumberOfEventsWrapper;
   beforeAll(() => {
-    numberFilter = shallow(<NumberOfEvents />);
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
   });
 
   test('render the number of events menu', () => {
-    expect(numberFilter.find('.numberOfEvents')).toHaveLength(1);
+    expect(NumberOfEventsWrapper.find('.numberOfEvents')).toHaveLength(1);
+  });
+
+  test("textbox element is rendered", () => {
+    expect(NumberOfEventsWrapper.find(".EventsNumber")).toHaveLength(1);
+  });
+
+  test("show 32 events by default", () => {
+    expect(NumberOfEventsWrapper.find(".EventsNumber").prop("value")).toEqual(
+      32
+    );
+  });
+
+  test("renders text input correctly", () => {
+    const numberOfEvents = NumberOfEventsWrapper.state("numberOfEvents");
+    expect(NumberOfEventsWrapper.find(".EventsNumber").prop("value")).toBe(
+      numberOfEvents
+    );
   });
 });
