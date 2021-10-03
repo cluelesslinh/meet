639a1d162e6901320f9b8ef8678435dbfc146a06
Linh Pham <77943437+cluelesslinh@users.noreply.github.com>
2021-10-03 13:02:08 -0700

    4.8 Welcome Screen
    update welcome screen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -30,7 +30,7 @@ o.svg"
           </div>
         </div>
         <a
-          href="https://YOUR_GITHUB_USERNAME.github.io/meet/privacy.html"
+          href="https://cluelesslinh.github.io/meet/privacy.html"
           rel="nofollow noopener"
         >
           Privacy policy
