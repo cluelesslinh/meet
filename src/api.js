d3e8f9b15a490bc3bb0cdc271907c431c9b3be64
Linh Pham <77943437+cluelesslinh@users.noreply.github.com>
2021-10-03 11:46:27 -0700

    4.8 PWA
    update get events function

--- src/api.js
+++ src/api.js
@@ -49,17 +49,16 @@ export const extractLocations = (events) => {
 
 export const getEvents = async () => {
   NProgress.start();
-  if (
-    !navigator.onLine &&
-    !window.location.href.startsWith('http://localhost')
-  ) {
-    const events = localStorage.getItem('lastEvents');
-    NProgress.done();
-    return JSON.parse(events).events;
-  }
-  if (window.location.href.startsWith('http://localhost')) {
+
+  if (window.location.href.startsWith("http://localhost")) {
     NProgress.done();
     return mockData;
+  }
+
+  if (!navigator.onLine) {
+    const events = localStorage.getItem('lastEvents');
+    NProgress.done();
+    return events ? JSON.parse(events).events : [];
   }
 
   const token = await getAccessToken();
