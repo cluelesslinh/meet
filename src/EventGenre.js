29031c7af0fec31e66c4b6de0c7d8d39aef5b256
Linh Pham <77943437+cluelesslinh@users.noreply.github.com>
2021-11-17 12:45:03 -0800

    Update EventGenre.js

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -4,8 +4,8 @@ import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from 'recharts';
 const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
 
-  const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS', 'Angular'];
-  const COLORS = ['#f2a365', '#BBE1FA', '#903749', '#219897', '#616f39', '#3B8EFF'];
+  const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS', 'Angular', 'Node.js'];
+  const COLORS = ['#f2a365', '#BBE1FA', '#903749', '#219897', '#616f39', '#3B8EFF', '#FF891A'];
 
 
   const getData = () => {
